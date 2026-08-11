6ab351352a1fb54a124567e726c2a10906babe1b
Coramen <dwk.03fyongfly_proton@proton.me>
2026-08-11 21:24:04 +0700

    Update [id].js

--- api/l/[id].js
+++ api/l/[id].js
@@ -3,7 +3,6 @@ const links = {
     // not generate by ai, bot.
     "ad92uda": "https://archive.org/details/en_windows_xp_professional_with_service_pack_3_x86_cd_vl_x14-73974_202108",
     "tfi6skg": "https://github.com/imearttchcorez",
-    "qw1fah8": "https://archive.org/details/windows-me-iso",
     "tr1akf5" :"https://example.net",
     "ytmtaec" :"https://www.youtube.com/embed/WkFJ4dR_DOY", // youtube music thinking about earttchcorez
     "sp1ca95" :"https://open.spotify.com/track/13ki1jkJsRkDzNep4aN0NZ", // random
@@ -11,6 +10,7 @@ const links = {
     "uca204m": "null",
     "kad29vz": "https://open.spotify.com/album/23lsZhoiLqnBC6SLlRkJPn", // new release
     "ec.spotify.artist" : "https://open.spotify.com/artist/7MbsIqyRcRoDsF87UVswrw" // my spotify artist
+   "ecl-web": "https://ecl-web.netlify.app" // static-page of eclinkweb.vercel.app
 };
 
 module.exports = (req, res) => {
