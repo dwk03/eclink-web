6bd49f342f0fe04d81b2e9fac11eaf1b6a203393
Coramen <dwk.03fyongfly_proton@proton.me>
2026-08-11 21:00:01 +0700

    Add new Spotify artist link to redirect API

--- api/l/[id].js
+++ api/l/[id].js
@@ -8,8 +8,9 @@ const links = {
     "ytmtaec" :"https://www.youtube.com/embed/WkFJ4dR_DOY", // youtube music thinking about earttchcorez
     "sp1ca95" :"https://open.spotify.com/track/13ki1jkJsRkDzNep4aN0NZ", // random
     "fja20cm": "https://music.amazon.com/albums/B0HCP6TNK6" // amazon music
-     "uca204m": "null",
+    "uca204m": "null",
     "kad29vz": "https://open.spotify.com/album/23lsZhoiLqnBC6SLlRkJPn" // new release
+    "ec.spotify.artist" : "https://open.spotify.com/artist/7MbsIqyRcRoDsF87UVswrw" // my spotify artist
 };
 
 module.exports = (req, res) => {
