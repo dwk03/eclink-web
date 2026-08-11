9d1b41ab8ff970f887a5f26e4f067d76cee1d91b
Coramen <dwk.03fyongfly_proton@proton.me>
2026-08-11 21:23:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const http = require("http");
 
 const links = {
     "ad92uda": "https://archive.org/details/en_windows_xp_professional_with_service_pack_3_x86_cd_vl_x14-73974_202108",
-    "tfi6skg": "https://github.com/imearttchcorez",
-    "qw1fah8": "https://archive.org/details/windows-me-iso",
+    "tfi6skg": "https://github.com/imearttchcorez"
     "tr1akf5": "https://example.net",
     "ytmtaec" :"https://www.youtube.com/embed/WkFJ4dR_DOY", // youtube music thinking about earttchcorez
     "sp1ca95" :"https://open.spotify.com/track/13ki1jkJsRkDzNep4aN0NZ",// random
     "fja20cm": "https://music.amazon.com/albums/B0HCP6TNK6",
     "uca204m": "null",
     "kad29vz": "https://open.spotify.com/album/23lsZhoiLqnBC6SLlRkJPn",
-    "ec.spotify.artist" : "https://open.spotify.com/artist/7MbsIqyRcRoDsF87UVswrw" // my spotify artist
+    "ec.spotify.artist" : "https://open.spotify.com/artist/7MbsIqyRcRoDsF87UVswrw",// my spotify artist
+    "ecl-web": "https://ecl-web.netlify.app" // static-page of eclinkweb.vercel.app
 };
 
 const server = http.createServer((req, res) => {
