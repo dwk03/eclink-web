e33364935524aca102b62949f48a45f6363000cc
Coramen <dwk.03fyongfly_proton@proton.me>
2026-08-18 17:12:47 +0700

    Update [id].js

--- api/l/[id].js
+++ api/l/[id].js
@@ -2,15 +2,13 @@ const links = {
     // random link
     // not generate by ai, bot.
     "ad92uda": "https://archive.org/details/en_windows_xp_professional_with_service_pack_3_x86_cd_vl_x14-73974_202108",
-    "tfi6skg": "https://github.com/imearttchcorez",
-    "tr1akf5" :"https://example.net",
-    "ytmtaec" :"https://www.youtube.com/embed/WkFJ4dR_DOY", // youtube music thinking about earttchcorez
+    "ytmtaec" :"https://www.youtube.com/watch?v=WkFJ4dR_DOY", // youtube music thinking about earttchcorez
     "sp1ca95" :"https://open.spotify.com/track/13ki1jkJsRkDzNep4aN0NZ", // random
-    "fja20cm": "https://music.amazon.com/albums/B0HCP6TNK6", // amazon music
+    "amznsic1": "https://music.amazon.com/albums/B0HCP6TNK6", // amazon music
     "uca204m": "https://www.youtube.com/watch?v=SJFk-d0mxIo",
-    "kad29vz": "https://open.spotify.com/album/23lsZhoiLqnBC6SLlRkJPn", // new release
+    "kad29vz": "https://open.spotify.com/album/23lsZhoiLqnBC6SLlRkJPn", 
     "ec.spotify.artist" : "https://open.spotify.com/artist/7MbsIqyRcRoDsF87UVswrw", // my spotify artist
-   "ecl.web": "https://ecl-web.netlify.app" // static-pages of eclinkweb.vercel.app
+    "🍎🎵" : "https://music.apple.com/us/song/thinking-about/6802273553"
 };
 
 module.exports = (req, res) => {
